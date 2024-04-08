ed99bed3faad0805874861c5593cd0de96b944ba
Rusiru Jayanga <132242402+RusiruJayanga@users.noreply.github.com>
2024-04-08 17:13:44 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\Apps\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\NSBM\\2nd Year\\2nd Semester\\PUSL2023 - Mobile App Development\\Assessments\\Project\\pizza_app-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\Apps\\Flutter\\flutter"
+  "PROJECT_DIR=F:\\NSBM\\2nd Year\\2nd Semester\\PUSL2023 - Mobile App Development\\Assessments\\Project\\pizza_app-main"
+  "FLUTTER_ROOT=E:\\Apps\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\NSBM\\2nd Year\\2nd Semester\\PUSL2023 - Mobile App Development\\Assessments\\Project\\pizza_app-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\NSBM\\2nd Year\\2nd Semester\\PUSL2023 - Mobile App Development\\Assessments\\Project\\pizza_app-main"
+  "FLUTTER_TARGET=F:\\NSBM\\2nd Year\\2nd Semester\\PUSL2023 - Mobile App Development\\Assessments\\Project\\pizza_app-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lNzZjOTU2NDk4ODQxZTFhYjQ1ODU3N2QzODkyMDAzZTU1M2U0ZjNjLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\NSBM\\2nd Year\\2nd Semester\\PUSL2023 - Mobile App Development\\Assessments\\Project\\pizza_app-main\\.dart_tool\\package_config.json"
+)
